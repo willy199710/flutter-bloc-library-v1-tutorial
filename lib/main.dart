d499928d38448175bb997ab6f64f538c2bc5a0f8
willy199710 <54666980+willy199710@users.noreply.github.com>
2022-07-28 14:59:13 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
       home: WeatherSearchPage(),
     );
   }
